0b7ac105d83d33aa0562606b83bb562f0826e3e8
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2026-02-23 20:22:37 -0300

    tests: criacao de testes unitarios e pipeline github actions

--- src/api/v1/routes/medical_records.py
+++ src/api/v1/routes/medical_records.py
@@ -16,58 +16,85 @@ from src.core.deps import get_session
 router = APIRouter()
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=MedicalRecordSchema)
-async def post_medical_record(medical_record: MedicalRecordSchema, db: AsyncSession = Depends(get_session)):
+@router.post(
+    "/", status_code=status.HTTP_201_CREATED, response_model=MedicalRecordSchema
+)
+async def post_medical_record(
+    medical_record: MedicalRecordSchema, db: AsyncSession = Depends(get_session)
+):
     new_medical_record: MedicalRecordsModel = MedicalRecordsModel(
-        appointment_id = medical_record.appointment_id,
-        vet_id = medical_record.vet_id,
-        diagnosis = medical_record.diagnosis,
-        treatment = medical_record.treatment,
-        prescriptions = [p.model_dump() for p in medical_record.prescriptions] if medical_record.prescriptions else None,
-        follow_up_date = medical_record.follow_up_date,
-        created_at = medical_record.created_at,
-        updated_at = medical_record.updated_at
+        appointment_id=medical_record.appointment_id,
+        vet_id=medical_record.vet_id,
+        diagnosis=medical_record.diagnosis,
+        treatment=medical_record.treatment,
+        prescriptions=[p.model_dump() for p in medical_record.prescriptions] if medical_record.prescriptions else None,
+        follow_up_date=medical_record.follow_up_date,
+        created_at=medical_record.created_at,
+        updated_at=medical_record.updated_at,
     )
-    
+
     db.add(new_medical_record)
     await db.commit()
-    
+
     return new_medical_record
 
-@router.get("/", status_code=status.HTTP_200_OK, response_model=List[MedicalRecordSchema])
+
+@router.get(
+    "/", status_code=status.HTTP_200_OK, response_model=List[MedicalRecordSchema]
+)
 async def get_medical_records(db: AsyncSession = Depends(get_session)):
     async with db as session:
         query = select(MedicalRecordsModel)
         results = await session.execute(query)
-        
+
         medical_records = results.scalars().unique().all()
-        
+
         return medical_records
-    
+
+
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=MedicalRecordSchema)
-async def get_medical_record(medical_record_id: int, db: AsyncSession = Depends(get_session)):
+async def get_medical_record(
+    medical_record_id: int, db: AsyncSession = Depends(get_session)
+):
     async with db as session:
-        query = select(MedicalRecordsModel).filter(MedicalRecordsModel.id == medical_record_id)
+        query = select(MedicalRecordsModel).filter(
+            MedicalRecordsModel.id == medical_record_id
+        )
         result = await session.execute(query)
-        
+
         medical_record = result.scalars().unique().one_or_none()
-        
+
         if not medical_record:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Medical Record not found.")
-        
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Medical Record not found.",
+            )
+
         return medical_record
-    
-@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=MedicalRecordSchema)
-async def put_medical_record(medical_record_id: int, medical_record: MedicalRecordSchema, db: AsyncSession = Depends(get_session)):
+
+
+@router.put(
+    "/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=MedicalRecordSchema
+)
+async def put_medical_record(
+    medical_record_id: int,
+    medical_record: MedicalRecordSchema,
+    db: AsyncSession = Depends(get_session),
+):
     async with db as session:
-        query = select(MedicalRecordsModel).filter(MedicalRecordsModel.id == medical_record_id)
+        query = select(MedicalRecordsModel).filter(
+            MedicalRecordsModel.id == medical_record_id
+        )
         result = await session.execute(query)
-        
+
         medical_record_up: MedicalRecordsModel = result.scalars().unique().one_or_none()
-        
+
         if not medical_record_up:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Medical Record not found.")
-        
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Medical Record not found.",
+            )
+
         if medical_record:
             if medical_record.appointment_id:
                 medical_record_up.appointment_id = medical_record.appointment_id
@@ -78,32 +105,46 @@ async def put_medical_record(medical_record_id: int, medical_record: MedicalReco
             if medical_record.treatment:
                 medical_record_up.treatment = medical_record.treatment
             if medical_record.prescriptions:
-                medical_record_up.prescriptions = [p.model_dump() for p in medical_record.prescriptions]
+                medical_record_up.prescriptions = [
+                    p.model_dump() for p in medical_record.prescriptions
+                ]
             if medical_record.follow_up_date:
                 medical_record_up.follow_up_date = medical_record.follow_up_date
             if medical_record.created_at:
                 medical_record_up.created_at = medical_record.created_at
             if medical_record.updated_at:
                 medical_record_up.updated_at = medical_record.updated_at
-        
+
         await session.commit()
-        
+
         return medical_record_up
 
+
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_medical_record(medical_record_id: int, db: AsyncSession = Depends(get_session)):
+async def delete_medical_record(
+    medical_record_id: int, db: AsyncSession = Depends(get_session)
+):
     async with db as session:
-        query = select(MedicalRecordsModel).filter(MedicalRecordsModel.id == medical_record_id)
+        query = select(MedicalRecordsModel).filter(
+            MedicalRecordsModel.id == medical_record_id
+        )
         result = await session.execute(query)
-        
+
         medical_record_del = result.scalars().unique().one_or_none()
-        
+
         if not medical_record_del:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Medical Record not found.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Medical Record not found.",
+            )
 
         await session.delete(medical_record_del)
         await session.commit()
-        
-        return Response(content="Medical record deleted successfully.", status_code=status.HTTP_204_NO_CONTENT)
+
+        return Response(
+            content="Medical record deleted successfully.",
+            status_code=status.HTTP_204_NO_CONTENT,
+        )
+
 
 # TODO Exibir prontuários de um animal
